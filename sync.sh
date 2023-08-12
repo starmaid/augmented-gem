413e7dbb285839aad8afda386b3aa706f1873b7f
starmaid <nicholas.masso.14@gmail.com>
2023-08-12 13:59:00 -0700

    created sync file

--- sync.sh
+++ sync.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+STARTDIR=`pwd`
+CUSER=`whoami`
+
+# check to make sure we are in repo root (unless you change the folder name)
+if [[ "$STARTDIR" =~ .*\/augmented-gem$ ]]
+then
+  echo "In git root folder. Continuing."
+else
+  echo "Not in git root folder. Exiting."
+  exit
+fi
+
 # relative to repo root
 ARTASSETS="./gem/Assets/Art/"
-GDRIVE="../gamejam_art/"
+GDRIVE="../gamejam_gdrive/"
 
-STARTDIR=`pwd`
+GITDATE=`date +"%y-%m-%d-%H%M"`
+BRANCHNAME="auto/art-$GITDATE"
+
+git checkout main
+git pull
+git checkout -b $BRANCHNAME
 
 # get the files from google drive.
 # rclone is a program that needs some one-time setup
@@ -13,3 +30,7 @@ rclone sync -v --create-empty-src-dirs "gamejam_art:Gamejam 2023" $STARTDIR/$GDR
 # sync between the directories
 rsync -av --include "*/" --include="*.png" --include="*.wav" --exclude="*" $STARTDIR/$GDRIVE $STARTDIR/$ARTASSETS
 
+git add .
+git commit -m "Auto-update of art assets by $CUSER's computer"
+
+#git push --set-upstream origin $BRANCHNAME
