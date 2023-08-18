2232bae00ce67decaf35a0504ed9b46dde7c6d01
Glacial <glacierkato@gmail.com>
2023-08-18 18:22:46 -0400

    had 2 kill my son 2day :(

---
+++ gem/Assets/TileD/pain and suffering (and wednesday)/tile_set_temp_tiles.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tile_set_temp_tiles" tilewidth="48" tileheight="48" tilecount="224" columns="16">
+ <image source="pixel_dungeon_48x48.png" width="768" height="672"/>
+ <tile id="42">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8" y="24" width="32" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="58">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="1" width="44" height="45"/>
+  </objectgroup>
+ </tile>
+ <tile id="135">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="48" height="48"/>
+  </objectgroup>
+ </tile>
+ <tile id="136">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="48" height="48"/>
+   <object id="2" x="0" y="0" width="48" height="48"/>
+  </objectgroup>
+ </tile>
+</tileset>
